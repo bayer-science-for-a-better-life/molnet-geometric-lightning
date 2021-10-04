5e48d2092ed3e4971bb09df2692c37d245d8a07f
gltyk <ryan.henderson@bayer.com>
2021-10-04 13:57:20 +0200

    initial commit

---
+++ setup.py
@@ -0,0 +1,7 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='molnet-geometric-lightning',
+    version='0.1.0',
+    packages=find_packages(include=['molnet-geometric-lightning', 'molnet-geometric-lightning.*'])
+)
